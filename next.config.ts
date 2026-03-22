3fb496a8e027303e71e3b6b66189520052b3582f
Tushar Fulmali <tushar.fulmali@outlook.com>
2026-03-22 17:01:32 +0530

    tailingSlash issue

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "tushar-fulmali";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
+  trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `${repoName}` : "",
+  /* config options here
+  New-Item "out/.nojekyll" -ItemType File
+  */
 };
 
 export default nextConfig;
