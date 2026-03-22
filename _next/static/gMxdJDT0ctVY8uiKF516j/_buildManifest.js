58e264dc9493d2c7487a40c19ca382a8fc86b86f
Tushar Fulmali <tushar.fulmali@outlook.com>
2026-03-22 17:37:05 +0530

    Updates

--- _next/static/gMxdJDT0ctVY8uiKF516j/_buildManifest.js
+++ _next/static/gMxdJDT0ctVY8uiKF516j/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/assetPrefix/_next/:path+",
+        "destination": "/basePath/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
